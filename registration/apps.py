818ad09f0c15b3636c5e3bf6eead3d569b637ec0
romeo <romeomeshack34@gmail.com>
2023-01-11 12:30:49 +0100

    Modified

--- registration/apps.py
+++ registration/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
+from registration.authentication import CustomAuthBackendSchema
 
 
 class RegistrationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'registration'
+
+    def ready(self):
+        CustomAuthBackendSchema
--- registration/authentication.py
+++ registration/authentication.py
@@ -1,6 +1,7 @@
 import importlib
 
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from drf_spectacular.extensions import OpenApiAuthenticationExtension
 
 from common_app.utils.general_utils import app_settings
 
@@ -29,3 +30,17 @@ class CustomAuthBackend(JWTAuthentication):
             return user, validated_token
         else:
             return 
+
+class CustomAuthBackendSchema(OpenApiAuthenticationExtension):
+    target_class = "registration.authentication.CustomAuthBackend"
+    name = "Authentication schema"
+    priority = 1
+
+    def get_security_definition(self, auto_schema):
+        return {
+            "type": "http",
+            "description": "JWT Authentication token",
+            "name": "JWT token",
+            "in": "header",
+            # "scheme": ""
+       }
