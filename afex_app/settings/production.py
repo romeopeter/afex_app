2a3422937f88ed749cd15442d8255f013ba15e37
romeo <romeomeshack34@gmail.com>
2023-01-13 01:37:38 +0100

    Django whitenoise for serving assets via django

--- afex_app/settings/production.py
+++ afex_app/settings/production.py
@@ -1,11 +1,23 @@
 from .base import *
 import dj_database_url
 
-# DEBUG = False
-
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 
+MIDDLEWARE += [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
+
+# AWS settings
+STATIC_URL = "/static/"
+
+if not DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / "static"]
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    # MEDIA_URL = "/media/"
+    # MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(config("RENDER_DATABASE_URL"), conn_max_age=600)
 }
